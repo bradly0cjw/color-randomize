a5eeca997913c34fb3231324d9333f5a3381e0da
bradly0cjw <bradly093@gmail.com>
2022-06-01 16:16:58 +0800

    upload file

---
+++ 10625color/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.HScrollBar2 = New System.Windows.Forms.HScrollBar()
+        Me.HScrollBar3 = New System.Windows.Forms.HScrollBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(82, 216)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "黑"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(200, 216)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 43)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "白"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(311, 272)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(93, 44)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "結束"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Red
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("MiSans", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(130, 70)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(346, 109)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Bradly0cjw"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Lime
+        Me.Button4.Location = New System.Drawing.Point(311, 216)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(93, 43)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "綠"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Cyan
+        Me.Button5.Location = New System.Drawing.Point(419, 216)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(93, 43)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "藍"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.LargeChange = 15
+        Me.HScrollBar1.Location = New System.Drawing.Point(82, 330)
+        Me.HScrollBar1.Maximum = 269
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(430, 35)
+        Me.HScrollBar1.TabIndex = 7
+        '
+        'HScrollBar2
+        '
+        Me.HScrollBar2.LargeChange = 15
+        Me.HScrollBar2.Location = New System.Drawing.Point(82, 377)
+        Me.HScrollBar2.Maximum = 269
+        Me.HScrollBar2.Name = "HScrollBar2"
+        Me.HScrollBar2.Size = New System.Drawing.Size(430, 35)
+        Me.HScrollBar2.TabIndex = 8
+        '
+        'HScrollBar3
+        '
+        Me.HScrollBar3.LargeChange = 15
+        Me.HScrollBar3.Location = New System.Drawing.Point(82, 424)
+        Me.HScrollBar3.Maximum = 269
+        Me.HScrollBar3.Name = "HScrollBar3"
+        Me.HScrollBar3.Size = New System.Drawing.Size(430, 35)
+        Me.HScrollBar3.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(523, 330)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 28)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(523, 377)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 28)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(523, 424)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 28)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("新細明體", 12.0!)
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(49, 342)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(17, 16)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "R"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("新細明體", 12.0!)
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(48, 388)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(18, 16)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "G"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("新細明體", 12.0!)
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(48, 435)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(17, 16)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "B"
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.Location = New System.Drawing.Point(200, 272)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(93, 43)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "Random"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(622, 505)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.HScrollBar3)
+        Me.Controls.Add(Me.HScrollBar2)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "背景色"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents HScrollBar1 As HScrollBar
+    Friend WithEvents HScrollBar2 As HScrollBar
+    Friend WithEvents HScrollBar3 As HScrollBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button6 As Button
+End Class
---
+++ 10625color/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.BackColor = Color.Black
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.BackColor = Color.White
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label1.BackColor = Color.Lime
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Label1.BackColor = Color.Cyan
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        Label1.BackColor = Color.FromArgb(HScrollBar1.Value, HScrollBar2.Value, HScrollBar3.Value)
+        Label1.ForeColor = Color.FromArgb(255 - HScrollBar1.Value, 255 - HScrollBar2.Value, 255 - HScrollBar3.Value)
+        Label2.Text = HScrollBar1.Value
+    End Sub
+
+    Private Sub HScrollBar2_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar2.Scroll
+        Label1.BackColor = Color.FromArgb(HScrollBar1.Value, HScrollBar2.Value, HScrollBar3.Value)
+        Label1.ForeColor = Color.FromArgb(255 - HScrollBar1.Value, 255 - HScrollBar2.Value, 255 - HScrollBar3.Value)
+        Label3.Text = HScrollBar2.Value
+    End Sub
+
+    Private Sub HScrollBar3_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar3.Scroll
+        Label1.BackColor = Color.FromArgb(HScrollBar1.Value, HScrollBar2.Value, HScrollBar3.Value)
+        Label1.ForeColor = Color.FromArgb(255 - HScrollBar1.Value, 255 - HScrollBar2.Value, 255 - HScrollBar3.Value)
+        Label4.Text = HScrollBar3.Value
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim a, b, c As Integer
+        a = (Int(255) * Rnd() + 0)
+        b = (Int(255) * Rnd() + 0)
+        c = (Int(255) * Rnd() + 0)
+        Label1.BackColor = Color.FromArgb(a, b, c)
+        Label2.Text = a
+        Label3.Text = b
+        Label4.Text = c
+        HScrollBar1.Value = a
+        HScrollBar2.Value = b
+        HScrollBar3.Value = c
+        Label1.ForeColor = Color.FromArgb(255 - a, 255 - b, 255 - c)
+    End Sub
+End Class
